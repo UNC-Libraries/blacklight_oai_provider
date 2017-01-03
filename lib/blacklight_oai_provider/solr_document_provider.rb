adbb5c2782368a21c8f5c20c314f1203aaeca2be
Corey Hinshaw <hinshaw.25@osu.edu>
2017-01-03 15:19:55 -0500

    Move constantize call to wrapper_class option

--- lib/blacklight_oai_provider/solr_document_provider.rb
+++ lib/blacklight_oai_provider/solr_document_provider.rb
@@ -9,8 +9,8 @@ module BlacklightOaiProvider
       options[:provider][:repository_name] ||= controller.view_context.send(:application_name)
       options[:provider][:repository_url] ||= controller.view_context.send(:oai_provider_catalog_url)
 
-      wrapper_class = options[:provider].delete(:wrapper_class) || SolrDocumentWrapper
-      self.class.model = wrapper_class.constantize.new(controller, options[:document])
+      wrapper_class = options[:provider].delete(:wrapper_class).try(:constantize) || SolrDocumentWrapper
+      self.class.model = wrapper_class.new(controller, options[:document])
 
       options[:provider].each do |k, v|
         self.class.send k, v
